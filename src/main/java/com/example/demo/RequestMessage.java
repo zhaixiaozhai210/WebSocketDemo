74afce9523cdf9ff90e394575877e7f8087f9240
sunhaiyang <zhaixiaozhai210@gmail.com>
2019-07-23 19:26:43 +0800

    first commit

---
+++ src/main/java/com/example/demo/RequestMessage.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class RequestMessage {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/example/demo/WsController.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class WsController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+
+    @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
+    public WsController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/welcome")
+    @SendTo("/topic/say")
+    public ResponseMessage say(RequestMessage message) {
+        System.out.println(message.getName());
+        return new ResponseMessage("welcome," + message.getName() + " !");
+    }
+
+    /**
+     * 定时推送消息
+     */
+    @Scheduled(fixedRate = 1)
+    public void callback() {
+        // 发现消息
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        messagingTemplate.convertAndSend("/topic/callback", "定时推送消息时间: " + df.format(new Date()));
+    }
+
+    @MessageMapping("/getDeviceByBigClass")
+    public void getDeviceByBigClass(RequestMessage message) {
+        System.out.println(message.getName());
+        simpMessagingTemplate.convertAndSend("/topic/getDeviceByBigClass",message.getName());
+    }
+}
